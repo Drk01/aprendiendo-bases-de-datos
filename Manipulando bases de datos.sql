537e20e6f3269cb3f0bf70fe719429f66c0da696
Omar Monrroy <ogmonrroy@gmail.com>
2020-01-09 17:14:24 -0600

    Manipulando bases de datos

---
+++ Manipulando bases de datos.sql
@@ -0,0 +1,11 @@
+-- Creando base de datos.
+CREATE DATABASE "libreria_cf";
+
+-- Mostrando un listado de todas las bases de datos disponibles.
+SHOW DATABASES;
+
+-- Seleccionando base de datos.
+USE "libreria_cf";
+
+-- Eliminando base de datos.
+DROP DATABASE "libreria_cf";
