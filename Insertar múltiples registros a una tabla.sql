882494e9e5f495037918c27556271efe9e1d3d93
Omar Monrroy <ogmonrroy@gmail.com>
2020-01-10 03:45:17 -0600

    Insertar múltiples registros a una tabla

---
+++ Insertar múltiples registros a una tabla.sql
@@ -0,0 +1,12 @@
+INSERT INTO autor (
+    id,
+    nombre,
+    genero,
+    fecha_nacimiento,
+    pais_origen
+  )
+  VALUES
+  (1, 'Omar', 'M', '1000-10-10', 'México'),
+  (2, 'Omar', 'M', '1000-10-10', 'México'),
+  (3, 'Omar', 'M', '1000-10-10', 'México'),
+  (4, 'Omar', 'M', '1000-00-10', 'México');
