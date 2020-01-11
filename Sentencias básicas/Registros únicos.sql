e1851d2413a03b6de479b83d76634e46477bf2dd
Omar Monrroy <ogcm.kael@hotmail.com>
2020-01-11 07:26:59 -0600

    Registros únicos

---
+++ Sentencias básicas/Registros únicos.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS libreria_cf;
+
+CREATE DATABASE IF NOT EXISTS libreria_cf;
+
+USE libreria_cf;
+
+CREATE TABLE IF NOT EXISTS autor;
+
+CREATE TABLE libro;
+
+INSERT INTO
+    autor (
+        nombre,
+        apellido,
+        seudonimo,
+        fecha_de_nacimiento,
+        genero,
+        pais_origen
+    )
+VALUES
+    (
+        'Howard',
+        'Lovecraft',
+        'H.P. Lovecraft',
+        '1890-08-20',
+        'terror',
+        'USA'
+    );
+
+INSERT INTO
+    libro (id, titulo, fecha_de_publicacion)
+VALUES
+    (1, 'el llamado de Cthulhu', '1928-02-01');
+
+/* Registros únicos */
+SELECT
+    DISTINCT titulo
+FROM
+    libro;
